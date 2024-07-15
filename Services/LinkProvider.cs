42b04a271d45575ef5d9235c8c5d45b9e7f4f871
Hamza <156059525+HamzaZamreek@users.noreply.github.com>
2024-07-15 14:33:51 +0300

    added the link provider, which in the future will hold the links of all added repos

---
+++ Services/LinkProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoreanStore.Services
+{
+    public class LinkProvider
+    {
+        public static string RepoLink { get; set; } = "https://f-droid.org/repo/";
+    }
+}
